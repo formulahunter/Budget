b89590b4a75606003a9bbea7f008929d41ab672e
Hunter Gayden <gayden.hunter@gmail.com>
2020-10-17 14:16:13 -0400

    Reinitialize Vue app with 'vue create'

--- src/main.js
+++ src/main.js
@@ -1,39 +1,8 @@
-import Vue from 'vue';
-import BudgetApp from './components/BudgetApp.vue';
-import {instantiateSQL} from './util/utils.js';
+import Vue from 'vue'
+import App from './App.vue'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
-let vm = new Vue({
-  el: '#app',
-  data: {
-    records: undefined
-  },
-  template: '<BudgetApp :records="records"></BudgetApp>',
-  components: { BudgetApp },
-  created() {
-
-    fetch('/budget/data', {
-      method: 'GET',
-      headers: {
-        accept: 'application/json'
-      }
-    })
-    .then(res => {
-      if(res.status !== 200) {
-        console.debug(`server response status code: ${res.status}`);
-        console.debug('server response: %o', res);
-        throw new Error('bad server response');
-      }
-      return res.json();
-    })
-    .then(instantiateSQL)
-    .then(data => {
-      vm.records = data;
-    })
-    .catch(reason => {
-      console.error(`error loading app: ${reason}`);
-    });
-  }
-});
-
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
